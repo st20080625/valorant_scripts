d8240f50dc16e65d587ebabb3c8ea30aa8554599
soichiro <85546532+st20080625@users.noreply.github.com>
2024-11-29 23:50:49 +0900

    #

--- bot.py
+++ bot.py
@@ -1,42 +1,15 @@
 import numpy as np
-import keyboard
 import cv2
 import time
-import mss
-import random
-import threading
-from queue import Queue
+from mss import mss as mss_module
+from random import uniform
+import sys
 import win32api
 import win32con
-
-def screenshot(monitor_target, lower_bound, upper_bound, queue):
-    with mss.mss() as sct:
-        while True:
-            if keyboard.is_pressed('left ctrl'):
-                screenshot = sct.grab(monitor_target)
-                rgb_array = np.array(screenshot)
-                if rgb_array.shape[-1] == 4:
-                    rgb_array = rgb_array[:, :, :3]
-                hsv_array = cv2.cvtColor(rgb_array, cv2.COLOR_RGB2HSV)
-                mask = cv2.inRange(hsv_array, lower_bound, upper_bound)
-                queue.put(mask)
-
-def shot(queue):
-    while True:
-        if keyboard.is_pressed('left ctrl'):
-            if not queue.empty():
-                mask = queue.get()
-                if np.any(mask):
-                    keyboard.press_and_release('k')
-                    #win32api.keybd_event(win32con.MOUSEEVENTF_LEFTDOWN, 0,0,0)
-                    #win32api.keybd_event(win32con.MOUSEEVENTF_LEFTUP, 0,0,0)
-
 def rgb_to_hsv(color):
     rgb = np.uint8([[color]])
     hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
     return hsv[0][0]
-
-# 設定
 target_color_rgb = (128, 0, 128)
 target_color_hsv = rgb_to_hsv(target_color_rgb)
 monitor_target = {
@@ -49,14 +22,28 @@ hue_range = 10
 lower_bound = np.array([max(target_color_hsv[0] - hue_range, 0), 50, 50])
 upper_bound = np.array([min(target_color_hsv[0] + hue_range, 179), 255, 255])
 
-queue = Queue()
-
-screenshot_thread = threading.Thread(target=screenshot, daemon=True, args=(monitor_target, lower_bound, upper_bound, queue))
-shot_thread = threading.Thread(target=shot, daemon=True, args=(queue,))
-
-screenshot_thread.start()
-shot_thread.start()
-
+sct = mss_module()
+# keycode(16) 0xA2 = left ctrl
+# keycode(16) 0x50 = p
 while True:
-    if keyboard.is_pressed('p'):
-        break
+    if win32api.GetAsyncKeyState(0xA2) < 0:
+        print('program detected keyboard event')
+        screenshot = sct.grab(monitor_target)
+        rgb_array = np.array(screenshot)
+        if rgb_array.shape[-1] == 4:
+            rgb_array = rgb_array[:, :, :3]
+
+        hsv_array = cv2.cvtColor(rgb_array, cv2.COLOR_RGB2HSV)
+        mask = cv2.inRange(hsv_array, lower_bound, upper_bound)
+        if np.any(mask):
+            print('Is mask')
+            sleep_time = uniform(0.18, 0.24)
+            win32api.keybd_event(0x4B, 0, 0, 0)
+            #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0)
+            print('down')
+            time.sleep(sleep_time)
+            win32api.keybd_event(0x4B, 0, win32con.KEYEVENTF_KEYUP, 0)
+            #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0)
+            time.sleep(0.01)
+    if win32api.GetAsyncKeyState(0x50) < 0:
+        sys.exit()
